3407f3df7ebcf00232325e5fb2ea1de0fa818a77
sena kalabalık <46752539+senakalabalik@users.noreply.github.com>
2022-12-25 19:31:44 +0300

    Update gorev3.cpp

--- gorev3.cpp
+++ gorev3.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
+#include <bits/stdc++.h>
 
-template <typename T>
-vector create_matrix (T x, int row, int col) {
-  std:: vector<T> vec1
-  return matr,x.
+template <class T> 
+
+std::vector<std::vector<T> > create_matrix(T(x), int(rows), int(cols)) {
+	std::vector< std::vector<int> > matrix ;
+	std::vector<int>invector;
+		for (int j = 0; j < cols; j++) {
+			invector.push_back(x);
+		}
+	for (int i = 0; i < rows; i++) {
+		matrix.push_back(invector);
+	}
+	return matrix ;
+}
+
+
+int main() {
+
+	create_matrix(int(5),int(3),int(3))	; //örnek input
+}
 
